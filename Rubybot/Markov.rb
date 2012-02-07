f079da818908b06b5f7ef61c0c8f9ae90c920575
Andrew Marrone <github@andrewmarrone.com>
2012-02-06 16:14:03 -0800

    different method to connect to facebook with Mechanize

--- Rubybot/Markov.rb
+++ Rubybot/Markov.rb
@@ -3,11 +3,11 @@ require_relative 'SentenceGenerator.rb'
 
 if __FILE__ == $0
 
-		filename = ARGV[0] || 'dict/dict.json'
+		filename = ARGV[0] || "dict/dict.json"
     wordcount = ARGV[1] || 20
 
     data = PersistentDictionary.new
-    data.load(filename)
+    data.load('dict/' + filename)
     result = TwoSentenceGen.new(data.dictionary)
     (1..10).each { puts result.generate(wordcount.to_i-1) }
 
